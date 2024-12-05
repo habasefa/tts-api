857cb3ff5a4a64b0a582ee68369a88e0e72aa8ba
Eba Adisu <ebaadisu2@gmail.com>
2024-12-05 13:03:44 +0300

    made the secret for jwt for both admin and user the same

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-require("dotenv").config()
+require('dotenv').config()
 
 module.exports = {
     PORT: process.env.PORT,
-    SECRET: process.env.SECRET
+    SECRET: process.env.ACCESS_TOKEN_SECRET,
 }
