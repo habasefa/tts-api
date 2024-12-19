78189970c63d8e1db27c4fb601946895ae9a6c20
Eba Adisu <ebaadisu2@gmail.com>
2024-12-19 22:07:10 +0300

    tutor app url

--- Utils/url.js
+++ Utils/url.js
@@ -1,5 +1,6 @@
 // const APP_URL = 'localhost/3000'
 // const APP_URL =
 //     'https://tts-nextjs-xdtk-git-staging-habtamuasefas-projects.vercel.app/'
-const APP_URL = 'tutor.temari-bet.com'
+const APP_URL = 'https://tutor.temari-bet.com/'
+
 module.exports = { APP_URL }
