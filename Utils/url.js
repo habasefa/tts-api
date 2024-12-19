5d9dd1e1b1f1be9d6b1266151702abf12b260724
Eba Adisu <ebaadisu2@gmail.com>
2024-12-19 22:04:15 +0300

    Update APP_URL in Utils/url.js

--- Utils/url.js
+++ Utils/url.js
@@ -1,4 +1,5 @@
 // const APP_URL = 'localhost/3000'
-const APP_URL =
-    'https://tts-dashboard-ytzz-git-tts-v2-habtamuasefas-projects.vercel.app/'
+// const APP_URL =
+//     'https://tts-nextjs-xdtk-git-staging-habtamuasefas-projects.vercel.app/'
+const APP_URL = 'tutor.temari-bet.com'
 module.exports = { APP_URL }
